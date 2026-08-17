7bd0e7320c202fe12552fae59ff9c2c5cdc1739e
Nitish Gupta <nkdabur7@gmail.com>
2026-08-18 02:47:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,11 @@ import errorHandler from "./middleware/error.middleware.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.ALLOWED_ORIGINS.split(","),
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin: "https://mini-gram-client.vercel.app",
+   methods: ["GET", "POST"],
+    credentials: true
+}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
